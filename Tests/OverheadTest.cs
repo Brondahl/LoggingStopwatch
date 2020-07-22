89e69ce6739ea8d9fc0f50b91e7e01103fe32000
Mike.McLean <Mike.McLean@softwire.com>
2020-07-22 09:21:03 +0100

    Performance test

---
+++ Tests/OverheadTest.cs
@@ -0,0 +1,30 @@
+using LoggingStopwatch;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var total = 0;
+            using (var timer = new LongOperationLoggingStopwatch("test", _ => { }))
+            {
+                for (int i = 0; i < 10_000_000; i++)
+                {
+                    using (timer.TimeInnerOperation())
+                    {
+                        total++;
+                    }
+                }
+                
+            }
+        }
+    }
+}
